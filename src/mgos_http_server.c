72bebb8ed500a6a21d3035cc1a836453a6e2c1eb
dude-mansaa <39621383+dude-mansaa@users.noreply.github.com>
2018-06-11 17:54:59 +0530

    Update mgos_http_server.c

--- src/mgos_http_server.c
+++ src/mgos_http_server.c
@@ -212,7 +212,9 @@ static void mgos_http_ev(struct mg_connection *c, int ev, void *p,
         if(s_http_server_opts.document_root != NULL){
             struct http_message *hm = (struct http_message *)p;
             LOG(LL_INFO,("%p %. *s %. *s",c,(int)hm->method.len,hm->mthod.p,(int)hm->uri.len,hm->uri.p));
+          (void)hm;
         }
+      
 #endif
       {
         mg_http_send_error(c, 404, "Not Found");
